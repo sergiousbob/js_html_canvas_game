43f1e728efad04eec72009e62c717794176ca5e5
sergiousbob <ivanovfedya@gmail.com>
2023-02-05 14:12:13 +0500

    pull_game_comment1

---
+++ index.js
@@ -0,0 +1,518 @@
+const scoreEl = document.querySelector ('#scoreEl')
+const canvas = document.querySelector ('canvas')
+const c =canvas.getContext('2d')
+
+console.log (scoreEl)
+
+canvas.width=innerWidth
+canvas.height=innerHeight
+
+
+class Player{
+    constructor(){
+      
+        this.velocity ={
+            x:0,
+            y:0
+        }
+        this.rotation = 0
+        this.opacity=1
+
+        const image = new Image ()
+        image.src ='./img/cat.png'
+        image.onload=()=>{
+            const scale = 0.15
+            this.image=image
+            this.width=image.width *scale
+            this.height=image.height *scale
+            this.position ={
+                x:canvas.width /2 - this.width / 2,
+                y:canvas.height - this.height - 20
+            }
+        }
+
+    }
+        draw() {
+           /// c.fillStyle='red'
+           /// c.fillRect(this.position.x, this.position.y, this.width, this.height)
+           c.save ()
+           c.globalAlpha= this.opacity
+           c.translate(player.position.x + player.width /2,
+                        player.position.y+ player.height/2)
+            c.rotate(this.rotation)
+            c.translate(-player.position.x - player.width /2,
+            -player.position.y- player.height/2)
+           c.drawImage(
+                this.image, 
+                this.position.x,
+                 this.position.y,
+                 this.width,
+                 this.height)
+            c.restore ()
+        }
+
+
+    update () {
+        if (this.image){
+        this.draw()
+        this.position.x += this.velocity.x
+        } 
+    }
+}
+class Projectfile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity= velocity
+
+
+        this.radius = 3
+    }
+    draw (){
+        c.beginPath ()
+        c.arc (this.position.x, this.position.y, this.radius, 0, 
+            Math.PI*2)
+        c.fillStyle='#c7a714'
+        c.fill ()
+        c.closePath()
+    }
+    update (){
+        this.draw()
+        this.position.x +=this.velocity.x
+        this.position.y +=this.velocity.y
+    }
+}
+
+
+
+class InvaderProjectfile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity= velocity
+        this.width=3
+        this.height=10
+
+
+    
+    }
+    draw (){
+        c.fillStyle='#142265'
+     c.fillRect (this.position.x, this.position.y, this.width, this.height)
+    }
+    update (){
+        this.draw()
+        this.position.x +=this.velocity.x
+        this.position.y +=this.velocity.y
+    }
+}
+
+class Particle {
+    constructor({position, velocity, radius, color, fades}){
+        this.position = position
+        this.velocity= velocity
+
+
+        this.radius = radius
+        this.color = color
+       if( this.fades)
+        this.opacity -=  0.01
+        
+
+    }
+    draw (){
+        c.save ()
+        c.globalAlpha=this.opacity
+        c.beginPath ()
+        c.arc (this.position.x, this.position.y, this.radius, 0, 
+            Math.PI*2)
+        c.fillStyle=this.color
+        c.fill ()
+        c.closePath()
+        c.restore()
+    }
+    update (){
+        this.draw()
+        this.position.x +=this.velocity.x
+        this.position.y +=this.velocity.y
+
+        this.opacity -=0.01
+    }
+}
+
+
+class Invader{
+    constructor({position}){
+      
+        this.velocity ={
+            x:0,
+            y:0
+        }
+        
+
+        const image = new Image ()
+        image.src ='./img/rock.png'
+        image.onload=()=>{
+            const scale = 1
+            this.image=image
+            this.width=image.width *scale
+            this.height=image.height *scale
+            this.position ={
+                x: position.x,
+                y:position.y
+            }
+        }
+
+    }
+        draw() {
+           /// c.fillStyle='red'
+           /// c.fillRect(this.position.x, this.position.y, this.width, this.height)
+          
+           c.drawImage(
+                this.image, 
+                this.position.x,
+                 this.position.y,
+                 this.width,
+                 this.height)
+           
+        }
+
+
+    update ({velocity}) {
+        if (this.image){
+        this.draw()
+        this.position.x += velocity.x
+        this.position.y += velocity.y
+        } 
+    }
+    shoot(InvaderProjectfiles) {
+        InvaderProjectfiles.push(new InvaderProjectfile({
+            position: {
+                x: this.position.x +this.width / 2,
+                y: this.position.y + this.height
+            },
+            velocity: {
+                x: 0,
+                y: 5
+            }
+        }))
+
+
+    }
+}
+class Grid {
+    constructor(){
+        this.position ={
+            x:0,
+            y:0
+        }
+        this.velocity={
+            x:3,
+            y:0
+        }
+        this.invaders=[]
+        const columns =Math.floor (Math.random () * 10 + 5)
+        const rows =Math.floor (Math.random () * 5 + 2)
+
+        this.width = columns * 30
+
+
+        for (let x=0; x<columns; x++){
+            for (let y=0; y<rows; y++){
+            this.invaders.push (new Invader ({position: {
+             x:x * 30,
+             y: y*30  
+            }}))
+        }
+    }
+      
+    }
+    update(){
+        this.position.x +=this.velocity.x
+        this.position.y +=this.velocity.y
+
+
+        this.velocity.y=0
+        
+        if (this.position.x + this.width >= canvas.width ||
+            this.position.x <=0){
+            this.velocity.x = -  this.velocity.x
+            this.velocity.y = 30
+        }
+    }
+}
+
+
+const player = new Player()
+const projectfiles =[]
+const grids =[]
+const InvaderProjectfiles = []
+const particles = []
+
+const keys ={
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    },
+}
+
+let frames = 0
+let randomInterval = Math.floor(Math.random()*500  +500) 
+let game = {
+    over: false,
+    active: true
+}
+
+let score = 0
+
+for (let i=0; i<100; i++){
+    particles.push (new Particle ({
+        position: {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height
+        },
+        velocity:{
+            x: 0, 
+            y: 0.2
+        },
+        radius: Math.random()*2,
+        color: 'white'
+    }))}
+
+
+
+
+function createParticles({object, color, fades}){
+    
+    for (let i=0; i<15; i++){
+        particles.push (new Particle ({
+            position: {
+                x: object.position.x+ object.width/2,
+                y:object.position.y + object.height/2
+            },
+            velocity:{
+                x: (Math.random ()- 0.5)*2, 
+                y: (Math.random () -0.5) *2 
+            },
+            radius: Math.random()*3,
+            color: color || '#5fe0e9', ///взрыв камней
+            fades
+        }))}
+}
+
+
+
+function animate (){
+
+    if (!game.active) return 
+    requestAnimationFrame(animate)
+    c.fillStyle='#92d5ea'
+    c.fillRect(0, 0 , canvas.width, canvas.height)
+
+   player.update()
+   particles.forEach((particle, i) => {
+
+if (particle.position.y - particle.radius >= canvas.height ){
+    particle.position.x = Math.random()* canvas.width
+    particle.position.y = -particle.radius 
+}
+
+
+    if (particle.opacity<=0){
+        setTimeout( ()=> {
+            particles.splice (i, 1)
+        }, 0)
+        } else { particle.update ()}
+   
+   })
+   InvaderProjectfiles.forEach((InvaderProjectfile, index)=>{
+    if (InvaderProjectfile.position.y + InvaderProjectfile.height >= canvas.height){
+        setTimeout(() =>{
+            InvaderProjectfiles.splice (index, 1)
+        }, 0)
+    }else InvaderProjectfile.update()
+  if (InvaderProjectfile.position.y + InvaderProjectfile.height >=player.position.y
+    && InvaderProjectfile.position.x+ InvaderProjectfile.width >= player.position.x
+    && InvaderProjectfile.position.x <=player.position.x + player.width) {
+        ////console.log ('you lose!!!')
+        alert ('GAME OVER.Please enter F5 on your keyboard....')
+        score= 'GAME OVER'
+        setTimeout(() =>{
+            InvaderProjectfiles.splice (index, 1)
+            player.opacity = 0
+            game.over= true
+          
+            ////console.log (score)
+            scoreEl.innerHTML = score
+            ////result
+            
+        }, 0)
+
+
+        setTimeout(() =>{
+           game.active=false
+        }, 2000)
+     
+        createParticles  ({
+            object: player,
+            color: 'white',
+            fades: true 
+        })
+    }
+   })
+   projectfiles.forEach((projectfile, index) =>{
+    if (projectfile.position.y + projectfile.radius<=0){
+        setTimeout(() =>{
+            projectfiles.splice (index, 1)
+        }, 0)
+       
+    } else {
+        projectfile.update ()
+    }
+    
+   })
+
+ grids.forEach((grid, gridIndex)=>{
+    grid.update ()
+
+
+    if (frames %100 === 0 && grid.invaders.length>0) ///спавн снарядов
+{
+    grid.invaders [Math.floor(Math.random() * grid.invaders.length)].shoot (InvaderProjectfiles)
+}
+
+
+
+    grid.invaders.forEach ((invader, i) =>{
+        invader.update ({velocity: grid.velocity})
+        projectfiles.forEach((projectfile, j)=>{
+            if (projectfile.position.y -projectfile.radius<=
+                invader.position.y+ invader.height &&
+                projectfile.position.x + projectfile.radius >= 
+                invader.position.x && 
+                projectfile.position.x - projectfile.radius <= 
+                invader.position.x + invader.width && projectfile.position.y+
+                 projectfile.radius >=invader.position.y)
+            {
+
+                
+                setTimeout(()=>{
+                    const invaderFound = grid.invaders.find((invader2) =>invader2 === invader)
+
+                        const projectfileFound = projectfiles.find (
+                            projectfile2 => projectfile2 === projectfile)
+                        if (invaderFound && projectfileFound) {
+                                score+=100
+                                ////console.log (score)
+                                scoreEl.innerHTML = score
+                                
+
+
+                    createParticles  ({
+                        object: invader,
+                        fades:true
+                    })
+
+
+
+                    grid.invaders.splice (i, 1 )
+                    projectfiles.splice (j, 1 )
+
+                    if (grid.invaders.length >0){
+                        const firstInvader = grid.invaders [0]
+                        const lastInvader = grid.invaders [grid.
+                        invaders.length - 1]
+                        grid.width = lastInvader.position.x - 
+                        firstInvader.position.x + lastInvader.width
+                        grid.position.x = firstInvader.position.x
+                    } else {
+                        grids.splice(gridIndex, 1)
+                    }
+                        }
+                }, 0)
+            }
+        })
+    })
+ })
+
+
+   if (keys.a.pressed && player.position.x >= 0) {
+    player.velocity.x = -7
+   player.rotation = -0.15
+   }else if(keys.d.pressed && player.position.x + player.width <= canvas.width){
+    player.velocity.x = 7
+    player.rotation = 0.15
+   }
+   else {
+    player.velocity.x =0
+    player.rotation = 0
+   }
+  //// console.log (frames)
+   if (frames %randomInterval === 0){
+    grids.push (new Grid())
+    let randomInterval = Math.floor(Math.random()*500  +500) 
+    ///console.log(randomInterval)
+   }
+ 
+   frames++
+}
+
+animate()
+
+addEventListener(`keydown`, ({key})=>{
+    if (game.over)  return
+  
+    switch(key) {
+        case 'a':
+          ///  console.log ('left')
+           
+            keys.a.pressed= true
+            break
+        case 'd':
+            ////console.log ('right')
+            keys.d.pressed= true
+            break
+        case ' ':
+           //// console.log ('space')
+            keys.space.pressed= true
+            projectfiles.push( new Projectfile({
+                position:{
+                    x: player.position.x+ player.width/2,
+                    y: player.position.y
+                },
+                velocity:{
+                    x:0,
+                    y:-10
+                }
+            }))
+           //// console.log (projectfiles)
+            break
+    }
+}
+)
+
+
+
+addEventListener(`keyup`, ({key})=>{
+    
+  
+    switch(key) {
+        case 'a':
+           /// console.log ('left')
+           
+            keys.a.pressed= false
+            break
+        case 'd':
+           //// console.log ('right')
+            keys.d.pressed= false
+            break
+        case ' ':
+           //// console.log ('space')
+            keys.space.pressed= false
+            break
+    }
+}
+)
